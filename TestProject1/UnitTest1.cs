357feaf0ffeba2f44f61ae1b95b2ed5ba68d02a2
Arch <arkad@DESKTOP-1C4G5PI>
2025-06-11 12:52:26 +0300

    Tests

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using TrySqlServer;
+using TrySqlServer.Entity;
+namespace TestProject1
+{
+    public class Tests
+    {
+        [Test]
+        public void AddUserAndMovie()
+        {
+            // Очистити базу
+            using (AppDBContext context = new AppDBContext())
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                // Додати користувача
+                User user = new User
+                {
+                    Username = "testuser",
+                    Email = "test@test.com",
+                    Password = "password123"
+                };
+                context.Users.Add(user);
+
+                // Додати фільм
+                Movie movie = new Movie
+                {
+                    Title = "Test Movie",
+                    Description = "This is a test movie.",
+                    ReleaseYear = 2023,
+                    Users = user
+                };
+                context.Movie.Add(movie);
+                context.SaveChanges();
+            }
+
+            // Перевірити дані
+            using (AppDBContext context = new AppDBContext())
+            {
+                string username = context.Users.FirstOrDefault().Username;
+                string title = context.Movie.FirstOrDefault().Title;
+
+                Assert.That(username, Is.EqualTo("testuser"));
+                Assert.That(title, Is.EqualTo("Test Movie"));
+            }
+        }
+    }
+}
